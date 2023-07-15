ad6cc11224dddc696660021873915c6c6087345c
pritimunde1999 <pritimunde0109@gmail.com>
2023-07-15 13:41:04 +0530

    Done

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((value) =>{
+    if(value.profession === "developer")
+    {
+      console.log(value)
+    }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((value)=>{
+    if(value.profession === "developer")
+    {
+      console.log(value)
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+   arr.push({id:4,name:"susan",age:"20",profession:"intern"})
+
+   console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+   arr = arr.filter((value) =>{
+     return (value.profession != "admin")
+   })
+
+   console.log(arr)
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 =[
+    {id:10, fname:"priti", hobby:"cooking"},
+    {id:11, fname:"ankita", hobby:"dancing"},
+    {id:12, fname:"shraddha", hobby:"reading"}
+  ]
+
+  let result = arr.concat(arr1)
+  console.log(result)
 }
